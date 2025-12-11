2bc5fa23c3f79ca6c460bcaf908538e2a7a83c9f
broduoliviercontact-web <brodu.olivier.contact@gmail.com>
2025-12-11 16:02:34 +0100

    new commit

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ extended: true, limit: "3mb" }));
 app.use(cookieParser());
 
 // -------------------- HELPER TOKEN --------------------
